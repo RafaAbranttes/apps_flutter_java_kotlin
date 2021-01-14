c67f234f9fc1c62661434454f5d06b0a22f316ea
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-01-14 17:17:16 -0300

    app2 jokenpo no mesmo repositorio

---
+++ app2/jokenpo_app/lib/jogo.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('JokenPo'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text('Escolha do App'),
+          ),
+        ],
+      ),
+    );
+  }
+}
