4699a45e269a78a8b11630e828363f519ae4be57
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-01-18 21:48:18 -0300

    adicionando melhorias

--- app5/app_5/lib/home.dart
+++ app5/app_5/lib/home.dart
@@ -9,15 +9,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _valorCep = TextEditingController();
+
+  String logradouro;
+  String complemento;
+  String bairro;
+  String localidade;
+  String uf;
+
   Future<void> _recuperarCEP() async {
-    String cep = '01311300';
+    String cep = _valorCep.text;
     String url = 'http://viacep.com.br/ws/$cep/json/';
     http.Response response;
 
     response = await http.get(url);
     Map<String, dynamic> retorno = json.decode(response.body);
-    String estado = retorno['uf'];
-    print(estado);
+
+    setState(() {
+      logradouro = retorno['logradouro'];
+      complemento = retorno['complemento'];
+      bairro = retorno['bairro'];
+      localidade = retorno['localidade'];
+      uf = retorno['uf'];
+    });
+    print(retorno);
   }
 
   @override
@@ -30,10 +45,28 @@ class _HomeState extends State<Home> {
         padding: EdgeInsets.all(40),
         child: Column(
           children: [
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Digite o cep: ex: 05429100',
+              ),
+              style: TextStyle(fontSize: 20),
+              controller: _valorCep,
+            ),
             RaisedButton(
               child: Text('Clique aqui'),
               onPressed: _recuperarCEP,
-            )
+            ),
+            Column(
+              children: [
+                Text(
+                    logradouro == null ? 'Endereço:' : 'Endereço: $logradouro'),
+                Text(bairro == null ? 'Bairro' : 'Bairro: $bairro'),
+                Text(localidade == null && uf == null
+                    ? 'Estado'
+                    : 'Estado: $localidade - $uf'),
+              ],
+            ),
           ],
         ),
       ),
