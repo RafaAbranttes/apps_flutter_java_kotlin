ffe6ee20609001b3a370d77a4a314b7f20b8abf7
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-01-21 23:53:09 -0300

    Começando o app2 em kotlin - gasolina ou álcool

---
+++ app2_kotlin/app/src/main/java/com/estudoparajava/gasolinaoulcool/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.estudoparajava.gasolinaoulcool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
