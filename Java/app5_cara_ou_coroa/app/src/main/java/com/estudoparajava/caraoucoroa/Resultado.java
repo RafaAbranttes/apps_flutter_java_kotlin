1f686bd6c30513cb427c0a287d8193c3326733f5
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-01-29 16:48:58 -0300

    Arquivos organizados e app cara ou cora finalizado em java

---
+++ Java/app5_cara_ou_coroa/app/src/main/java/com/estudoparajava/caraoucoroa/Resultado.java
@@ -0,0 +1,40 @@
+package com.estudoparajava.caraoucoroa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Resultado extends AppCompatActivity {
+
+    private Button buttonVoltar;
+    private ImageView imageResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+        imageResultado = findViewById(R.id.imageView);
+
+        Bundle dados  = getIntent().getExtras();
+        int numero = dados.getInt("numero");
+
+        if(numero == 0){
+            imageResultado.setImageResource(R.drawable.moeda_cara);
+        }else {
+            imageResultado.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
